a0df8e70571ed64173d2e74ffc0634eb339d4db0
**123456Aa <Ahmeday5>
2026-07-02 15:40:02 +0300

    fix(environment): revert apiUrl to previous endpoint for stability

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://sehatytheone.runasp.net',
+  apiUrl: 'http://15.235.51.177/ShefaAPI',
   appName: 'سهاتي',
   appVersion: '1.0.0',
   currency: 'ج.م',
